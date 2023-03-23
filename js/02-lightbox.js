e628e5ade882f4cabce2f77b14e2b05bd65408a6
ivan-2908 <109952917+ivan-2908@users.noreply.github.com>
2023-03-23 15:11:21 +0200

    amendment

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,29 +1,23 @@
 import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
-
 const galleryEll = document.querySelector(".gallery");
 
 function createGalleryCardsMarkup(items) {
-  return items
-    .map(
-      ({ preview, original, description }) => `
-      <li class="gallery__item">
-        <a class="gallery__link"
-        href="${original}">
-      <img class="gallery__image" 
-          src="${preview}"
-          alt="${description}" />
-        </a>
-      </li>
-    `
-    )
-    .join("");
+  return items.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+  `).join("");
 }
 
 const galleryCardsMarkup = createGalleryCardsMarkup(galleryItems);
 galleryEll.insertAdjacentHTML("beforeend", galleryCardsMarkup);
 
-const lightbox = new SimpleLightbox('.gallery a', {
-     caption: true,
-      captionDelay: 250 });
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+console.log(galleryItems);
